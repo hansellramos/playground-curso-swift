4f6663bf26665347d42b97b47572291785f17257
hansellramos <hansell.ramos@gmail.com>
2016-02-14 12:14:15 -0500

    Create playground_Hansel_curso_swift.swift

---
+++ playground_Hansel_curso_swift.swift
@@ -0,0 +1,17 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var numeros = 0...100
+
+for numero in numeros {
+    if numero >= 30 && numero <= 40 {
+        print("\(numero) Viva Swift!!!")
+    }else if numero%5 == 0{
+        print("\(numero) Bingo!!!")
+    }else if numero%2 == 0 {
+        print("\(numero) Par!!!")
+    }else {
+        print("\(numero) Impar!!!")
+    }
+}
